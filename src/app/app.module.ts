fb415f4340e0a96a2c9b8d7d7e032d9a4d4fba9b
thiagorodrigues <thiagorodrigues1203@outlook.com>
2024-02-03 23:21:24 -0300

    Criando o formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
